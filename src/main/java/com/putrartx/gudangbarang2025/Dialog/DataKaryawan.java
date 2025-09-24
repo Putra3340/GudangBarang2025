23ee06fab1d25faa64898488d90969447e36f9b6
Putra3340 <putraerer3@gmail.com>
2025-09-24 18:09:21 +0700

    Update DBContext credentials and enhance DataKaryawan dialog layout

--- src/main/java/com/putrartx/gudangbarang2025/Dialog/DataKaryawan.java
+++ src/main/java/com/putrartx/gudangbarang2025/Dialog/DataKaryawan.java
@@ -5,19 +5,22 @@
 package com.putrartx.gudangbarang2025.Dialog;
 
 import com.putrartx.gudangbarang2025.DBContext;
-import static com.putrartx.gudangbarang2025.Dialog.DataBarangDialog.fetchBarangFromDB;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JTable;
+import javax.swing.RowFilter;
 import javax.swing.SwingUtilities;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
 
 /**
  *
@@ -30,6 +33,7 @@ public class DataKaryawan extends javax.swing.JDialog {
     /**
      * Creates new form DataKaryawan
      */
+    private String OperationCode;
     public DataKaryawan(JFrame parent) {
         super(parent, "Data Barang",true);
         initComponents();
@@ -53,7 +57,7 @@ DefaultComboBoxModel<String> model2 = new DefaultComboBoxModel<>();
 
     cbx_borncity.setModel(model);
     cbx_city.setModel(model2);
-    
+    ResetState();
 
 } catch (SQLException e) {
     e.printStackTrace();
@@ -61,25 +65,79 @@ DefaultComboBoxModel<String> model2 = new DefaultComboBoxModel<>();
         loadkaryawanToTable(frame);
         
         
-        new Thread(() -> {
-    while (true) {
-        try {
-            // Fetch data from DB
-            DefaultTableModel model = loadkaryawanToTableTh(frame);
+//        new Thread(() -> {
+//    while (true) {
+//        try {
+//            // Fetch data from DB
+//            DefaultTableModel model = loadkaryawanToTableTh(frame);
+//
+//            // Update JTable safely on EDT
+//            SwingUtilities.invokeLater(() -> {
+//                frame.setModel(model);
+//            });
+//
+//            // Refresh every 3 seconds
+//            Thread.sleep(3000);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}).start();
+frame.addMouseListener(new java.awt.event.MouseAdapter() {
+    @Override
+    public void mouseClicked(java.awt.event.MouseEvent evt) {
+        if(btn_input.getText() == "SAVE"){ // Block Action when inputting
+            btn_delete.setEnabled(false);
+            btn_search.setEnabled(false);
+            btn_update.setEnabled(false);
+            return;
+        }
+        btn_cancel.setEnabled(true);
+        btn_input.setEnabled(false);
+        btn_update.setEnabled(true);
+        btn_search.setEnabled(true);
+        btn_delete.setEnabled(true);
+        btn_search.setEnabled(false);
+        int row = frame.rowAtPoint(evt.getPoint());
+        int col = frame.columnAtPoint(evt.getPoint());
+        if (row >= 0 && col >= 0) {
+            // Get value directly from model
+            Object value = frame.getModel().getValueAt(row, col);
+            System.out.println("Clicked value: " + value);
 
-            // Update JTable safely on EDT
-            SwingUtilities.invokeLater(() -> {
-                frame.setModel(model);
-            });
+            // If you want the whole row data
+            int columnCount = frame.getColumnCount();
+//            for (int i = 0; i < columnCount; i++) {
+//                Object cell = frame.getModel().getValueAt(row, i);
+//                System.out.println("Column " + i + ": " + cell);
+//                if(i == 2)
+//            }
+                Object code = frame.getModel().getValueAt(row, 2);
+                OperationCode = code.toString();
+                
+                
+                
+                // Enable Input
+                enableInput();
+                // Fill Input
+                tbx_code.setEnabled(false);
+                tbx_tglinput.setText((frame.getModel().getValueAt(row, 1)).toString());
+                tbx_code.setText((frame.getModel().getValueAt(row, 2)).toString());
+                tbx_name.setText((frame.getModel().getValueAt(row, 3)).toString());
+                cbx_borncity.setSelectedItem((frame.getModel().getValueAt(row, 4)).toString());
+                try {
+    String dateStr = frame.getModel().getValueAt(row, 5).toString();
+    jDateChooser1.setDate(new SimpleDateFormat("yyyy-MM-dd").parse(dateStr));
+} catch (Exception e) {
+    e.printStackTrace(); // or handle properly
+}
 
-            // Refresh every 3 seconds
-            Thread.sleep(3000);
-
-        } catch (Exception e) {
-            e.printStackTrace();
+                cbx_city.setSelectedItem((frame.getModel().getValueAt(row, 6)).toString());
+                jTextArea1.setText((frame.getModel().getValueAt(row, 7)).toString());
         }
     }
-}).start();
+});
     }
 
     /**
@@ -107,8 +165,13 @@ DefaultComboBoxModel<String> model2 = new DefaultComboBoxModel<>();
         frame = new javax.swing.JTable();
         jLabel8 = new javax.swing.JLabel();
         tbx_tglinput = new javax.swing.JTextField();
-        btn_insert = new javax.swing.JButton();
+        btn_cancel = new javax.swing.JButton();
         cbx_city = new javax.swing.JComboBox<>();
+        btn_input = new javax.swing.JButton();
+        btn_update = new javax.swing.JButton();
+        btn_delete = new javax.swing.JButton();
+        tbx_search = new javax.swing.JTextField();
+        btn_search = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -162,10 +225,40 @@ DefaultComboBoxModel<String> model2 = new DefaultComboBoxModel<>();
         jLabel8.setText("Tanggal Input");
         jLabel8.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
 
-        btn_insert.setText("INPUT");
-        btn_insert.addActionListener(new java.awt.event.ActionListener() {
+        btn_cancel.setText("CANCEL");
+        btn_cancel.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btn_insertActionPerformed(evt);
+                btn_cancelActionPerformed(evt);
+            }
+        });
+
+        btn_input.setText("INPUT");
+        btn_input.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_inputActionPerformed(evt);
+            }
+        });
+
+        btn_update.setText("UPDATE");
+        btn_update.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_updateActionPerformed(evt);
+            }
+        });
+
+        btn_delete.setText("DELETE");
+        btn_delete.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_deleteActionPerformed(evt);
+            }
+        });
+
+        tbx_search.setToolTipText("Nama Barang...");
+
+        btn_search.setText("CARI");
+        btn_search.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_searchActionPerformed(evt);
             }
         });
 
@@ -206,12 +299,24 @@ DefaultComboBoxModel<String> model2 = new DefaultComboBoxModel<>();
                                 .addGap(18, 18, 18)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 489, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 896, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(btn_insert)))
+                                    .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 896, javax.swing.GroupLayout.PREFERRED_SIZE)))
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(jLabel8, javax.swing.GroupLayout.PREFERRED_SIZE, 117, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(18, 18, 18)
-                                .addComponent(tbx_tglinput, javax.swing.GroupLayout.PREFERRED_SIZE, 252, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                                .addComponent(tbx_tglinput, javax.swing.GroupLayout.PREFERRED_SIZE, 252, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(182, 182, 182)
+                        .addComponent(btn_cancel)
+                        .addGap(18, 18, 18)
+                        .addComponent(btn_input)
+                        .addGap(18, 18, 18)
+                        .addComponent(btn_update)
+                        .addGap(18, 18, 18)
+                        .addComponent(btn_delete)
+                        .addGap(18, 18, 18)
+                        .addComponent(tbx_search, javax.swing.GroupLayout.PREFERRED_SIZE, 415, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(btn_search)))
                 .addContainerGap(57, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -250,11 +355,22 @@ DefaultComboBoxModel<String> model2 = new DefaultComboBoxModel<>();
                     .addComponent(cbx_city, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel9)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 152, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(30, 30, 30)
-                .addComponent(btn_insert)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 33, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel9)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 152, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 32, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(btn_cancel)
+                            .addComponent(btn_input)
+                            .addComponent(btn_update)
+                            .addComponent(btn_delete)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(tbx_search, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(btn_search))))
+                .addGap(31, 31, 31)
                 .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 281, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(46, 46, 46))
         );
@@ -262,14 +378,27 @@ DefaultComboBoxModel<String> model2 = new DefaultComboBoxModel<>();
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btn_insertActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_insertActionPerformed
-        if((btn_insert.getText()).equals("INPUT")){
+    private void btn_cancelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_cancelActionPerformed
+        ResetState();
+    }//GEN-LAST:event_btn_cancelActionPerformed
+
+    private void tbx_nameKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_tbx_nameKeyTyped
+         if (tbx_name.getText().length() > 20) {
+        evt.consume(); // prevent typing more
+        JOptionPane.showMessageDialog(null, "Nama maksimal 20 karakter");
+    }
+    }//GEN-LAST:event_tbx_nameKeyTyped
+
+    private void btn_inputActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_inputActionPerformed
+        if((btn_input.getText())=="INPUT"){
             enableInput();
             
             clearInput();
             tbx_tglinput.setText(LocalDate.now().format(
             DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-            btn_insert.setText("SAVE");
+            btn_input.setText("SAVE");
+            btn_cancel.setEnabled(true);
+            btn_search.setEnabled(false);
         }else{
             if(!validateInputs()){
                 return;
@@ -298,16 +427,100 @@ stmt.setString(2, tbx_code.getText()); stmt.setString(3, tbx_name.getText()); st
             loadkaryawanToTable(frame);
             
             disableInput();
-            btn_insert.setText("INPUT");
+            btn_input.setText("INPUT");
+            ResetState();
         }
-    }//GEN-LAST:event_btn_insertActionPerformed
+    }//GEN-LAST:event_btn_inputActionPerformed
 
-    private void tbx_nameKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_tbx_nameKeyTyped
-         if (tbx_name.getText().length() > 20) {
-        evt.consume(); // prevent typing more
-        JOptionPane.showMessageDialog(null, "Nama maksimal 20 karakter");
+    private void btn_updateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_updateActionPerformed
+        if (!validateInputs()) {
+        return; // stop kalau invalid
+        }
+        int confirm = JOptionPane.showConfirmDialog(
+            this,
+            "Apakah kamu yakin ingin mengubah data ini?",
+            "Konfirmasi Hapus",
+            JOptionPane.YES_NO_OPTION,
+            JOptionPane.WARNING_MESSAGE
+    );
+            if(confirm == JOptionPane.NO_OPTION){
+                return;
+            }
+        String sql = "UPDATE karyawan SET " +
+             "tanggal_input = ?, " +
+             "nama = ?, " +
+             "kota_lahir = ?, " +
+             "tanggal_lahir = ?, " +
+             "alamat_kota = ?, " +
+             "alamat_jalan = ? " +
+             "WHERE no_karyawan = ?";
+
+try (Connection conn = DBContext.getConnection();
+     PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+    // Example of setting parameters
+    stmt.setDate(1, java.sql.Date.valueOf(tbx_tglinput.getText())); // must be yyyy-MM-dd 
+stmt.setString(2, tbx_name.getText());
+stmt.setString(3, cbx_borncity.getSelectedItem().toString());
+stmt.setDate(4, new java.sql.Date(jDateChooser1.getDate().getTime()));
+stmt.setString(5, cbx_city.getSelectedItem().toString());
+stmt.setString(6, jTextArea1.getText().toString());
+stmt.setString(7, OperationCode);
+
+    int rowsAffected = stmt.executeUpdate();
+    System.out.println(rowsAffected + " row(s) updated.");
+}
+ catch (Exception ex) {
+    ex.printStackTrace();
+}
+        loadkaryawanToTable(frame);
+ResetState();
+    }//GEN-LAST:event_btn_updateActionPerformed
+
+    private void btn_deleteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_deleteActionPerformed
+        int confirm = JOptionPane.showConfirmDialog(
+            this,
+            "Apakah kamu yakin ingin menghapus data ini?",
+            "Konfirmasi Hapus",
+            JOptionPane.YES_NO_OPTION,
+            JOptionPane.WARNING_MESSAGE
+    );
+            if(confirm == JOptionPane.NO_OPTION){
+                return;
+            }
+        String sql = "DELETE FROM karyawan WHERE no_karyawan = ?";
+
+    try (Connection conn = DBContext.getConnection();
+         PreparedStatement stmt = conn.prepareStatement(sql)) {
+        
+        stmt.setString(1, OperationCode); // assuming OperationCode is your selected kode_barang
+        int affected = stmt.executeUpdate();
+
+        if (affected > 0) {
+            JOptionPane.showMessageDialog(this, "Data berhasil dihapus!");
+            // Optionally refresh your JTable here
+        } else {
+            JOptionPane.showMessageDialog(this, "Data tidak ditemukan.");
+        }
+
+    } catch (Exception e) {
+        e.printStackTrace();
+        JOptionPane.showMessageDialog(this, "Terjadi kesalahan saat menghapus data: " + e.getMessage());
     }
-    }//GEN-LAST:event_tbx_nameKeyTyped
+        loadkaryawanToTable(frame);
+        ResetState();
+    }//GEN-LAST:event_btn_deleteActionPerformed
+
+    private void btn_searchActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_searchActionPerformed
+        loadkaryawanToTable(frame);
+        btn_input.setEnabled(false);
+        btn_cancel.setEnabled(true);
+        TableRowSorter<TableModel> sorter = new TableRowSorter<>(frame.getModel());
+        frame.setRowSorter(sorter);
+
+        String keyword = tbx_search.getText(); // the string to search
+        sorter.setRowFilter(RowFilter.regexFilter("(?i)" + keyword, 3));
+    }//GEN-LAST:event_btn_searchActionPerformed
 
     private boolean validateInputs() {
     if (tbx_tglinput.getText().isEmpty()) {
@@ -388,6 +601,24 @@ tbx_tglinput.setText("");
         tbx_name.setEnabled(into);
         tbx_tglinput.setEnabled(into);
     }
+        private void ResetState(){
+    clearInput();
+        disableInput();
+        btn_cancel.setEnabled(false);
+        btn_delete.setEnabled(false);
+        btn_input.setEnabled(true);
+        btn_input.setText("INPUT");
+        btn_search.setEnabled(true);
+        btn_update.setEnabled(false);
+        tbx_search.setText("");
+        OperationCode = "";
+        loadkaryawanToTable(frame);
+        TableRowSorter<TableModel> sorter = new TableRowSorter<>(frame.getModel());
+frame.setRowSorter(sorter);
+
+String keyword = ""; // turn off filter
+sorter.setRowFilter(RowFilter.regexFilter("(?i)" + keyword, 3));
+}
     public static void loadkaryawanToTable(JTable table) {
     String[] columns = {
         "Id", "Tanggal Input", "No Karyawan", "Nama Karyawan",
@@ -471,7 +702,11 @@ tbx_tglinput.setText("");
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btn_insert;
+    private javax.swing.JButton btn_cancel;
+    private javax.swing.JButton btn_delete;
+    private javax.swing.JButton btn_input;
+    private javax.swing.JButton btn_search;
+    private javax.swing.JButton btn_update;
     private javax.swing.JComboBox<String> cbx_borncity;
     private javax.swing.JComboBox<String> cbx_city;
     private javax.swing.JTable frame;
@@ -488,6 +723,7 @@ tbx_tglinput.setText("");
     private javax.swing.JTextArea jTextArea1;
     private javax.swing.JTextField tbx_code;
     private javax.swing.JTextField tbx_name;
+    private javax.swing.JTextField tbx_search;
     private javax.swing.JTextField tbx_tglinput;
     // End of variables declaration//GEN-END:variables
 }
